5f74dfcb583d6543b15108897474eb82b7abc944
Jay Gajera <jaygajera2001@gmail.com>
2022-12-30 11:02:57 +0530

    05

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
+// const express=require("express");
+// const app=express();
+// const path=require("path")
+
+// const staticPath=path.join(__dirname,"/invitation");
+
+// app.use(express.static(staticPath));
+
+// app.get("/Savaliya_family",(req,res)=>{
+//     res.sendFile(__dirname+"/invitation/savaliya_family.html");
+// });
+
+
+// app.get("/Sorathiya_family",(req,res)=>{
+//     res.sendFile(__dirname+"/sorathiya_family.html");
+// });
+// // app.get("",(req,res)=>{
+// //     res.render("index");
+// // });
+// app.listen(3000)
+
+
+
 const express=require("express");
 const app=express();
 const path=require("path")
+const router = express.Router();
 
-const staticPath=path.join(__dirname,"/invitation");
+const staticPath=path.join(__dirname,"./invitation");
 
 app.use(express.static(staticPath));
 
-app.get("/Savaliya_family",(req,res)=>{
-    res.sendFile(__dirname+"/invitation/savaliya_family.html");
-});
+// app.get("/Savaliya_family",(req,res)=>{
+//     res.sendFile(__dirname+"/savaliya_family.html");
+// });
 
 
-app.get("/Sorathiya_family",(req,res)=>{
-    res.sendFile(__dirname+"/sorathiya_family.html");
-});
+// app.get("/Sorathiya_family",(req,res)=>{
+//     res.sendFile(__dirname+"/invitation/sorathiya_family.html");
+// });
 // app.get("",(req,res)=>{
 //     res.render("index");
 // });
-app.listen(3000)
+// app.listen(8000)
+
+
+router.get('/Savaliya',function(req,res){
+  res.sendFile(path.join(__dirname+'/invitation/savaliya_family.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 9000);
+
+console.log('Running at Port 9000');
